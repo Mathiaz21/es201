897a250c3a7dbb12aebd351b4ba58311e7934638
clocheret <edouard.clocheret@ensta-paris.fr>
2024-02-13 14:47:10 +0100

    mofification mineure de simul_auto.py

--- simul-auto.py
+++ simul-auto.py
@@ -14,20 +14,22 @@ def find_number_by_name(text, name):
     else:
         return None  # Retourne None si le nom n'est pas trouvé dans le texte
 
-# Exécution de la commande à l'aide de subprocess et récupération de sa sortie
-result = subprocess.run(['python', 'test_sortie.py'], capture_output=True, text=True)
 
-# Exemple d'utilisation
-if result.returncode == 0:  # Vérification si la commande s'est exécutée avec succès
-    text_output = result.stdout  # Récupération de la sortie de la commande
-    name = "param1"
-    number = find_number_by_name(text_output, name)
-    if number is not None:
-        print("Le nombre associé à '{}' est : {}".format(name, number))
+def simple_example ():
+    # Exécution de la commande à l'aide de subprocess et récupération de sa sortie
+    result = subprocess.run(['python', 'test_sortie.py'], capture_output=True, text=True)
+
+    # Exemple d'utilisation
+    if result.returncode == 0:  # Vérification si la commande s'est exécutée avec succès
+        text_output = result.stdout  # Récupération de la sortie de la commande
+        name = "param1"
+        number = find_number_by_name(text_output, name)
+        if number is not None:
+            print("Le nombre associé à '{}' est : {}".format(name, number))
+        else:
+            print("Le nom '{}' n'a pas été trouvé.".format(name))
     else:
-        print("Le nom '{}' n'a pas été trouvé.".format(name))
-else:
-    print("Erreur lors de l'exécution de la commande.")
+        print("Erreur lors de l'exécution de la commande.")
 
 
 def simulation_1():
