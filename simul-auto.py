111ccf8a2367ed3a345bc32f9e4cdf33b13be08b
clocheret <edouard.clocheret@ensta-paris.fr>
2024-02-13 14:43:35 +0100

    definition de simulation1

--- simul-auto.py
+++ simul-auto.py
@@ -1,5 +1,7 @@
 import subprocess
 import re
+import matplotlib.pyplot as plt
+import numpy as np
 
 def find_number_by_name(text, name):
     # Pattern pour extraire le nombre associé au nom spécifié
@@ -28,3 +30,32 @@ else:
     print("Erreur lors de l'exécution de la commande.")
 
 
+def simulation_1():
+    #variable que l'on veut plot
+    name = "param1"
+    #abscisse1
+    x1=[12, 15, 17]
+    #abscisse2
+    x2=[18, 15, 17]
+    #ordonnée
+    y=np.zeros(len(x1))
+
+    valeur1 = x1[0]
+    for valeur2 in x2:
+        command = ["sim-cache", "-cache:{valeur1}", "il1:{valeur2}","toto.ss"]
+
+        # Exécution de la commande avec subprocess.run()
+        result = subprocess.run(command, capture_output=True, text=True)
+        
+        if result.returncode == 0:  
+            text_output = result.stdout
+            number = find_number_by_name(text_output, name)
+            if number is not None:
+                print("Le nombre associé à '{}' est : {}".format(name, number))
+                y.append(number)
+            else:
+                print("Le nom '{}' n'a pas été trouvé.".format(name))
+        else:
+            print("Erreur lors de l'exécution de la commande.")
+
+    plt.plot(y,x2)
